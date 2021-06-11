ed940770fad34a98f16ce6e16f16f8c1643533bd
Harper Rex Grimsley <hrgrimsl@tinkercliffs1.cm.cluster>
2021-06-11 15:15:33 -0400

    hi luke

--- computational_tools.py
+++ computational_tools.py
@@ -1,10 +1,73 @@
 import scipy
 import numpy as np
 import copy
+
+import time
+import os
 from opt_einsum import contract
 
+from multiprocessing import Pool
+from num2words import num2words
 
 
+
+def oo_vqe(H, ansatz, singles, ref, params, knorm = 1e-8, tnorm = 1e-8, Etol = 1e-12):
+
+    #Could be generalized so that no singles corresponds to normal VQE, no doubles is UCCSD?
+    times = [time.time()]
+    print("\n Orbital-optimized VQE:")
+    H_eff = copy.copy(H) #H transformed by the orbital optimization
+    psi = copy.copy(ref) #|0> transformed by the actual unitary in the VQE
+    Done = False
+
+    E_history = []
+    E_history.append(simple_energy(params, H_eff, ansatz, ref))
+    k_history = [] #list of previous orbital optimization params
+
+    k_history.append(np.zeros(len(singles)))
+    t_history = []
+    t_history.append(params)
+    iters = 0
+    times.append(time.time())
+    print(f"Initial energy: {E_history[-1]}")
+    while Done == False:
+        iters += 1
+        print(f"\n Iteration {iters}:")
+        print("Performing VQE with current orbitals:")
+
+        E_vqe, t, g, E0, Na = simple_vqe(H_eff, ansatz, ref, t_history[-1])
+        print(f"Energy:   {E_vqe}")
+        print(f"dE:       {E_vqe-E_history[-1]}")
+        print(f"dt norm:  {np.linalg.norm(t-t_history[-1])}")
+        print(f"gnorm:    {np.linalg.norm(g)}")
+        t_history.append(t)
+        psi = copy.deepcopy(ref)
+        for i in reversed(range(0, len(ansatz))):
+            psi = scipy.sparse.linalg.expm_multiply(ansatz[i]*t[i], psi)
+        print(f"Reoptimizing Orbitals")
+        E_oo, k, g_oo, E0 = simple_uccsd(H, singles, psi, k_history[-1])
+        print(f"Energy: {E_oo}")
+        print(f"dE:       {E_oo - E_vqe}")
+        print(f"dk norm:  {np.linalg.norm(k-k_history[-1])}")
+        print(f"gnorm:    {np.linalg.norm(g_oo)}")
+        E_history.append(E_oo)
+        k_history.append(k)
+        gen = 0*ansatz[0]
+        for i in range(0, len(singles)):
+            gen += singles[i]*k[i]
+        U = scipy.sparse.linalg.expm(gen)
+        H_eff = U.T@(H)@U
+        if np.linalg.norm(k-k_history[-2]) < knorm and np.linalg.norm(t-t_history[-2]) < tnorm and np.linalg.norm(E_oo - E_history[-2]) < Etol:
+            print("Converged.")
+            Done = True
+        times.append(time.time())
+        print(f"Iteration completed in {times[-1]-times[-2]} seconds.")
+    print(f"\nOO-VQE completed in {times[-1]-times[0]} seconds.\n")
+    return E_history[-1], list(t_history[-1]), g, E_history[0], U
+         
+        
+    
+    
 
 def product_hessian(params, H, ansatz, ref):
     #Can this be made faster by just storing things?
@@ -62,13 +125,36 @@ def product_hessian(params, H, ansatz, ref):
         pass
     return hessian
 
-def ML_M(params, H, ansatz, ref):       
+def ML_M(params, H, ansatz, ref):
+    N = len(ansatz)
+    M = np.zeros((N, N))
+    V = np.zeros((N))        
+    _ket = copy.copy(ref)
+    vec = []
+    for u in reversed(range(0, N)):
+        _ket = scipy.sparse.linalg.expm_multiply(params[u]*ansatz[u], _ket)
+        uket = ansatz[u]@_ket
+        vec = [(_ket.T@uket).todense()[0,0]] + vec
+        _uket = copy.copy(uket)
+        _vket = copy.copy(_ket)
+        for v in reversed(range(0, u+1)):
+            targ = scipy.sparse.hstack([_uket, _vket])
+            res = scipy.sparse.linalg.expm_multiply(params[v]*ansatz[v], targ).tocsr() 
+            _uket = res[:,0]
+            _vket = res[:,1]
+            M[u,v] = M[v,u] = -2*(_vket.T@(ansatz[v]@_uket)).todense()[0,0]             
+    for u in reversed(range(0, N)):
+        for v in reversed(range(0, u+1)):
+            M[u,v] += 2*vec[u]*vec[v]
+            if u != v:
+                M[v,u] += 2*vec[u]*vec[v]
+    return M
+    
+def ML_M_dumb(params, H, ansatz, ref):       
     #Can this be made faster by just storing things?
     N = len(ansatz)
     M = np.zeros((N, N))
     
-
-
     state = copy.copy(ref)
     for i in reversed(range(0, len(ansatz))):
         state = scipy.sparse.linalg.expm_multiply(params[i]*ansatz[i], state)
@@ -143,6 +229,38 @@ def product_energy(params, H, ansatz, ref):
             pass
     #print(E)
     return E
+
+def simple_energy(params, H, ansatz, ref):
+    state = copy.deepcopy(ref)
+    for i in reversed(range(0, len(ansatz))):
+        state = scipy.sparse.linalg.expm_multiply(params[i]*ansatz[i], state)
+    E = (state.T@(H)@state).todense()[0,0]
+    return E
+
+def simple_uccsd_energy(params, H, ansatz, ref):
+    gen = params[0]*ansatz[0]
+    for i in range(1, len(ansatz)):
+        gen += params[i]*ansatz[i]
+    state = scipy.sparse.linalg.expm_multiply(gen, ref)
+    E = (state.T@(H)@state).todense()[0,0]
+    return E
+
+def simple_gradient(params, H, ansatz, ref):
+    grad = []
+    ket = copy.copy(ref)
+
+    for i in reversed(range(0, len(ansatz))):
+        ket = scipy.sparse.linalg.expm_multiply(params[i]*ansatz[i], ket)
+
+    hbra = (H@ket).T
+    for i in range(0, len(ansatz)):
+        grad.append(2*(hbra@ansatz[i]@ket).todense()[0,0])
+        targ = scipy.sparse.hstack([hbra.T, ket])
+        res = scipy.sparse.linalg.expm_multiply(-params[i]*ansatz[i], targ).tocsr()
+        hbra = res[:,0].T
+        ket = res[:,1]
+
+    return np.array(grad)
 
 def sum_energy(params, H, ansatz, ref):
     state = copy.deepcopy(ref)
@@ -295,48 +413,197 @@ def ML_X(params, H, ansatz, ref):
 
     
 
-def VQITE(H, ansatz, ref, params, dt = .5, tol = 1e-8):
+def VQITE(H, ansatz, ref, params, dt = 1e-1, tol = 1e-10):
     print("Doing a McLachlan Imaginary Time Evolution")
-    V = -product_gradient(params, H, ansatz, ref)
+    #V = -product_gradient(params, H, ansatz, ref)
     iter = 0
     old_E = product_energy(params, H, ansatz, ref)
-    while np.linalg.norm(V) > tol:
+    V = None
+    hf = copy.copy(old_E)
+    E = copy.copy(old_E)
+    dE = 0
+    print(f"Iter.       |Energy              |dE                  |Grad Norm")
+    while (V is None or np.linalg.norm(V) > tol): 
+        V = -product_gradient(params, H, ansatz, ref)
+        print(f"{iter:12d}|{E:20.16f}|{dE:20.8e}|{np.linalg.norm(V):20.8e}")
+
+        if np.linalg.norm(V) < tol:
+            break
         iter += 1
         M = ML_M(params, H, ansatz, ref)
-        V = -product_gradient(params, H, ansatz, ref)
-        params += (dt*np.linalg.inv(M)@V)
+        #try:
+        #    direction = np.linalg.inv(M)@V
+        #except:
+        #    print("Singular matrix M, using pinv")
+        #    direction = np.linalg.pinv(M, rcond=1e-12)@V
+        direction = np.linalg.pinv(M, rcond = 1e-10)@V
+        old_params = copy.copy(params)
+        params += dt*direction
         E = product_energy(params, H, ansatz, ref)
         dE = E - old_E
+        dt_cur = copy.copy(dt)        
         old_E = copy.copy(E)
-        print(f"Iter.       |Energy              |dE                  |Grad Norm")
-        print(f"{iter:12d}|{E:20.16f}|{dE:20.8e}|{np.linalg.norm(V):20.8e}")
+
     return E, list(params)
          
 
-def vqe(H, ansatz, ref, params, gtol = 1e-8):
-    print("Doing VQE...")
-    global iters 
-    iters = 1
-    global xk 
-    xk = copy.copy(params)
-    #Stores data as params: [E, grad, hess]
-    global vqe_cache
-    vqe_cache = {}
-    vqe_cache['tuple(params)'] = [None, None, None]
-    Done = False
-    res = scipy.optimize.minimize(product_energy, np.array(params), jac = product_gradient, method = 'bfgs', callback = prod_cb, args = (H, ansatz, ref), options = {'gtol': gtol, 'disp': True, 'verbose': 4})
-    if res.success != True:
-        res.fun, res.x = VQITE(H, ansatz, ref, res.x, tol = gtol)
-    grad = vqe_cache[tuple(res.x)][1]
-    hess = vqe_cache[tuple(res.x)][2]
-    if grad is None:
-        grad = product_gradient(res.x, H, ansatz, ref)
-    if hess is None:
-        hess = product_hessian(res.x, H, ansatz, ref)
-    s, V = scipy.linalg.eigh(hess)
-    print(f"Smallest singular value: {s[0]:20.8e}")
+   
+def one_param_energy(param, H, ansatz, ref, param_no, params):
 
-    return res.fun, list(res.x), np.linalg.norm(grad), np.linalg.cond(hess)
+    state = copy.deepcopy(ref)
+    for i in reversed(range(0, len(ansatz))):
+        if i != param_no:
+            state = scipy.sparse.linalg.expm_multiply(params[i]*ansatz[i], state)
+        else:
+            state = scipy.sparse.linalg.expm_multiply(param[0]*ansatz[i], state)
+    E = (state.T@(H)@state).todense()[0,0]
+
+    return E
+
+def one_param_grad(param, H, ansatz, ref, param_no, params):
+    state = copy.deepcopy(ref)
+    istate = copy.deepcopy(ref)
+    for i in reversed(range(0, len(ansatz))):
+        if i != param_no:
+            istate = scipy.sparse.linalg.expm_multiply(params[i]*ansatz[i], state)
+            state = scipy.sparse.linalg.expm_multiply(params[i]*ansatz[i], state)
+        else:
+            istate = scipy.sparse.linalg.expm_multiply(param[0]*ansatz[i], state)
+            istate = ansatz[i]@istate
+            state = scipy.sparse.linalg.expm_multiply(param[0]*ansatz[i], state)
+    g = 2*(state.T@(H)@istate).todense()[0,0]
+    return np.array([g])
+
+
+
+def best_vqe(results, dump_dir = 'dump'):
+    Es = [i[0] for i in results]
+    xs = [i[1] for i in results]
+    gs = [i[2] for i in results]
+    E0s = [i[3] for i in results]
+
+    recycled = f"{len(xs[0])} {Es[0]} {np.linalg.norm(np.array(gs[0]))} {E0s[0]}"       
+    dump = f"./{dump_dir}/converged_recycled_{len(xs[0])}"
+    np.save(dump, np.array(xs[0]))
+    print(f"Random Initializations ({dump_dir}):")   
+    print("Seed | Operators | Energy | Gradient | Initial E | Recycled Data")
+    for e in range(1, len(Es)):
+        print(f"{e-1} {len(xs[0])} {Es[e]} {np.linalg.norm(np.array(gs[e]))} {E0s[e]} {recycled}")
+        dump = f"./{dump_dir}/converged_seed_{e}_{len(xs[0])}"
+        np.save(dump, np.array(xs[e]))
+
+    idx = np.argsort(Es)
+    print(f"Summary of Multi-initialized VQE with {len(xs[0])} operators:")
+    print(f"Recycled parameters give the {num2words(list(idx).index(0)+1, to='ordinal_num')} best energy.")
+    print(f"This energy is worse than the best one by:  {Es[idx[0]]- Es[0]}")
+    print(f"Energy min: {Es[idx[0]]}")
+    print(f"Energy max: {Es[idx[-1]]}")
+    print(f"Energy mean:  {np.mean(np.array(Es))}") 
+    print(f"Energy StdDev:  {np.std(np.array(Es))}")     
+    return Es[idx[0]], list(xs[idx[0]]), np.array(gs[idx[0]]), E0s[idx[0]], None
+
+def pre_sample(H, ansatz, ref, params, seeds = 10000, norm = 1):
+    Es = []
+    new_params = []
+
+    print("Pre-sampling energies about the given parameters:") 
+    for i in range(0, seeds):
+
+        guess_vec = list(params + 20*np.random.random_sample(len(ansatz))-10)
+        new_params.append(guess_vec)
+        E = simple_energy(guess_vec, H, ansatz, ref)
+        Es.append(E)
+        print(f"Seed {i}: {E}")
+    idx = np.argsort(np.array(Es))
+    return [Es[i] for i in idx][:125], [new_params[i] for i in idx][:125]
+
+def sample_uccsd(H, ansatz, ref, params, gtol = 1e-16, seeds = 125, dump_dir = 'dump_entangled'):
+    print("UCCSD ansatz sampling")
+    print("Recycled parameters:")
+    print(params)
+    if os.path.exists(dump_dir):
+        os.system(f'rm -r {dump_dir}')
+    os.makedirs(dump_dir)
+    start = time.time()
+    Es = []
+    gnorms = []
+    opt_params = []
+    os.system('export OPENBLAS_NUM_THREADS=1')
+    os.environ['OPENBLAS_NUM_THREADS'] = '1'
+    param_tuple = [list(params)]
+    dump = f"./{dump_dir}/recycled_{len(ansatz)}"
+    np.save(dump, np.array(params))
+    hf = np.zeros(len(ansatz))
+    #pre_Es, new_params = pre_sample(H, ansatz, ref, hf)
+    #param_tuple += new_params
+    for i in range(0, 125):
+        np.random.seed(i)
+        guess_vec = list(2*np.random.random_sample(len(ansatz))-1)
+        param_tuple.append(guess_vec)
+        #guess_vec = param_tuple[i+1]
+        dump = f"./{dump_dir}/guess_{i}_{len(ansatz)}"
+        np.save(dump, np.array(guess_vec))
+    with Pool(127) as p:
+        L = p.starmap(simple_uccsd, iterable = [*zip([H for i in range(0, 126)], [ansatz for i in range(0, 126)], [ref for i in range(0, 126)], param_tuple)])
+    assert(len(param_tuple) == len(L))
+    print(f"Time elapsed over whole set of optimizations: {time.time() - start}")
+    best = best_vqe(L, dump_dir = dump_dir)
+    best_params = best[1]
+    print("Optimized parameters:")
+    print(best_params)
+    return best[0], best[1]
+
+def sample_vqe(H, ansatz, ref, params, gtol = 1e-16, seeds = 125, dump_dir = 'dump_disentangled'):
+    if os.path.exists(dump_dir):
+        os.system(f'rm -r {dump_dir}')
+    os.makedirs(dump_dir)
+    start = time.time()
+    Es = []
+    gnorms = []
+    opt_params = []
+    os.system('export OPENBLAS_NUM_THREADS=1')
+    os.environ['OPENBLAS_NUM_THREADS'] = '1'
+    param_tuple = [list(copy.copy(params))]
+    dump = f"./{dump_dir}/recycled_{len(ansatz)}"
+    np.save(dump, np.array(params))
+    hf = np.zeros(len(ansatz))
+    #pre_Es, new_params = pre_sample(H, ansatz, ref, hf)
+    #param_tuple += new_params
+    for i in range(0, 125):
+        np.random.seed(i)
+        guess_vec = list(2*np.random.random_sample(len(ansatz))-1)
+        param_tuple.append(guess_vec)
+        #guess_vec = param_tuple[i+1]
+        dump = f"./{dump_dir}/guess_{i}_{len(ansatz)}"
+        np.save(dump, np.array(guess_vec))
+    with Pool(127) as p:
+        L = p.starmap(simple_vqe, iterable = [*zip([H for i in range(0, 126)], [ansatz for i in range(0, 126)], [ref for i in range(0, 126)], param_tuple)])
+    assert(len(param_tuple) == len(L))
+
+    print(f"Time elapsed over whole set of optimizations: {time.time() - start}")
+    return best_vqe(L, dump_dir = dump_dir)
+
+def simple_vqe(H, ansatz, ref, params, gtol = 1e-16):
+    E0 = simple_energy(params, H, ansatz, ref) 
+    res = scipy.optimize.minimize(simple_energy, np.array(params), jac = simple_gradient, method = 'bfgs', args = (H, ansatz, ref), options = {'gtol': gtol})
+    grad = simple_gradient(res.x, H, ansatz, ref)
+    return res.fun, res.x, grad, E0, None
+
+def simple_uccsd(H, ansatz, ref, params, gtol = 1e-16):
+    E0 = simple_energy(params, H, ansatz, ref) 
+    res = scipy.optimize.minimize(simple_uccsd_energy, np.array(params), jac = None, method = 'bfgs', args = (H, ansatz, ref), options = {'gtol': gtol})
+    grad = res.jac
+    return res.fun, res.x, grad, E0
+
+def vqe(H, ansatz, ref, params, gtol = 1e-16, singles = []):
+    with Pool(1) as p:
+        if len(singles) == 0:
+            L = p.starmap(simple_vqe, iterable = [*zip([H], [ansatz], [ref], list([params]))])
+        else:
+            return oo_vqe(H, ansatz, singles, ref, params)
+            
+    return best_vqe(L)
+
 
 def resid(x, H, ansatz, ref):
     grad = product_gradient(x, H, ansatz, ref)
