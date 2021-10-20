650efb0c0a9946ab27ca1a15df71afd2ff32deb7
Harper Rex Grimsley <hrgrimsl@tinkercliffs1.cm.cluster>
2021-10-20 10:22:15 -0400

    adapt square maybe

--- driver.py
+++ driver.py
@@ -659,17 +659,7 @@ def t_ucc_jac(params, ansatz, H_vqe, pool, ref):
     return J.real
 
 
-def vqe(params, ansatz, H_vqe, pool, ref, strategy = "bfgs", energy = None):
-    if energy is None or energy == t_ucc_E:
-        energy = t_ucc_E
-        jac = t_ucc_grad
-        hess = t_ucc_hess
 
-    if strategy == "newton-cg":
-        res = scipy.optimize.minimize(energy, params, jac = jac, hess = hess, method = "newton-cg", args = (ansatz, H_vqe, pool, ref), options = {'xtol': 1e-16})
-    if strategy == "bfgs":
-        res = scipy.optimize.minimize(energy, params, jac = jac, method = "bfgs", args = (ansatz, H_vqe, pool, ref), options = {'gtol': 1e-8})
-    return res
 
 def adapt_vqe(ansatz, H_vqe, pool, ref):
     params = []
@@ -707,16 +697,148 @@ def multi_vqe(params, ansatz, H_vqe, pool, ref, xiphos, energy = None, guesses =
         seeds.append(seed)
         np.random.seed(seed)
         param_list.append(math.pi*2*np.random.rand(len(params)))
-        E0s.append(energy(param_list[-1], ansatz, H_vqe, pool, ref))
+        #E0s.append(energy(param_list[-1], ansatz, H_vqe, pool, ref))
 
-    iterable = [*zip(param_list, [ansatz for i in range(0, len(param_list))], [H_vqe for i in range(0, len(param_list))], [pool for i in range(0, len(param_list))], [ref for i in range(0, len(param_list))])] 
-    with Pool(1) as p:
-        L = p.starmap(vqe, iterable = iterable)
+    #iterable = [*zip(param_list, [ansatz for i in range(0, len(param_list))], [H_vqe for i in range(0, len(param_list))], [pool for i in range(0, len(param_list))], [ref for i in range(0, len(param_list))], E0s, [xiphos for i in range(0, len(param_list))]] 
+    iterable = [*zip(param_list, [ansatz for i in range(0, len(param_list))], seeds, [xiphos for i in range(0, len(param_list))])]
+    with Pool(126) as p:
+        L = p.starmap(detailed_vqe, iterable = iterable)
     print(f"Time elapsed over whole set of optimizations: {time.time() - start}")
-    params = solution_analysis(L, ansatz, H_vqe, pool, ref, seeds, param_list, E0s, xiphos)
+    #params = solution_analysis(L, ansatz, H_vqe, pool, ref, seeds, param_list, E0s, xiphos)
+    params = L[0][0].x
+    idx = np.argsort([L[i][0].fun for i in range(0, len(L))])
+    for i in idx:
+        print(L[i][1], flush = True)
     return params
 
-    
+def multi_vqe_square(params, ansatz, H_vqe, pool, ref, xiphos, energy = None, guesses = 0):
+    from multiprocessing import Pool
+    start = time.time()
+    os.system('export OPENBLAS_NUM_THREADS=1')
+    os.environ['OPENBLAS_NUM_THREADS'] = '1'
+    if energy is None or energy == t_ucc_E:
+        energy = t_ucc_E
+        jac = t_ucc_grad
+        hess = t_ucc_hess
+    param_list = [np.array(list(0*params)+list(copy.copy(params)))]
+    seeds = ['Recycled']
+    E0s = [energy(params, ansatz, H_vqe, pool, ref)]
+    for i in range(0, guesses):
+        seed = (.5+i+guesses*(len(params)-1))
+        seeds.append(seed)
+        np.random.seed(seed)
+        param_list.append(math.pi*2*np.random.rand(2*len(params)))
+
+    iterable = [*zip(param_list, [ansatz for i in range(0, len(param_list))], seeds, [xiphos for i in range(0, len(param_list))])]
+    with Pool(126) as p:
+        L = p.starmap(detailed_vqe_square, iterable = iterable)
+    print(f"Time elapsed over whole set of optimizations: {time.time() - start}")
+    #params = solution_analysis(L, ansatz, H_vqe, pool, ref, seeds, param_list, E0s, xiphos)
+    params = L[0][0].x
+    idx = np.argsort([L[i][0].fun for i in range(0, len(L))])
+    for i in idx:
+        print(L[i][1], flush = True)
+    return params
+
+def detailed_vqe_square(params, ansatz, seed, xiphos):
+    energy = t_ucc_E
+    jac = t_ucc_grad
+    hess = t_ucc_hess
+    x0 = params
+    E0 = energy(params, ansatz, xiphos.H_vqe, xiphos.pool, xiphos.ref)
+    res = scipy.optimize.minimize(energy, params, jac = jac, method = "bfgs", args = (ansatz, xiphos.H_vqe, xiphos.pool, xiphos.ref), options = {'gtol': 1e-8})
+    EF = res.fun
+    gradient = t_ucc_grad(res.x, ansatz, xiphos.H_vqe, xiphos.pool, xiphos.ref)
+    jacobian = t_ucc_jac(res.x, ansatz, xiphos.H_vqe, xiphos.pool, xiphos.ref)
+    hessian = t_ucc_hess(res.x, ansatz, xiphos.H_vqe, xiphos.pool, xiphos.ref)
+    gnorm = np.linalg.norm(gradient)
+    u, s, vh = np.linalg.svd(jacobian.todense())
+    w, v = np.linalg.eigh(hessian)
+    state = t_ucc_state(res.x, ansatz, xiphos.pool, xiphos.ref)
+    fid = ((xiphos.ed_wfns[:,0].T)@state)[0].real**2
+    string = "\nSolution Analysis:\n\n"
+    string += f"Doubled Parameters: {len(ansatz)}\n"
+    string += f"Initialization: {seed}\n"
+    string += f"Initial Energy: {E0:20.16f}\n"
+    string += f"Final Energy: {EF:20.16f}\n"
+    string += f"GNorm: {gnorm:20.16f}\n"
+    string += f"Fidelity: {fid:20.16f}\n"
+    string += f"Solution Parameters:\n"
+    for x in res.x:
+        string += f"{x},"
+    string += "\n"
+    string += f"Jacobian Singular Values:\n"
+    for sv in s:
+        string += f"{sv},"
+    string += "\n"
+    string += f"Hessian Eigenvalues:\n"
+    for sv in w:
+        string += f"{sv},"
+    string += "\n"
+    string += f"Operator/ Expectation Value/ Error:\n"
+    for key in xiphos.sym_ops.keys():
+        val = ((state.T)@(xiphos.sym_ops[key]@state))[0,0].real
+        err = val - xiphos.ed_syms[0][key]
+        string += f"{key:<6}:      {val:20.16f}      {err:20.16f}\n"
+    string += '\n\n'
+    return [res, string]
+
+
+def vqe(params, ansatz, H_vqe, pool, ref, strategy = "bfgs", energy = None):
+    if energy is None or energy == t_ucc_E:
+        energy = t_ucc_E
+        jac = t_ucc_grad
+        hess = t_ucc_hess
+    if strategy == "newton-cg":
+        res = scipy.optimize.minimize(energy, params, jac = jac, hess = hess, method = "newton-cg", args = (ansatz, H_vqe, pool, ref), options = {'xtol': 1e-16})
+    if strategy == "bfgs":
+        res = scipy.optimize.minimize(energy, params, jac = jac, method = "bfgs", args = (ansatz, H_vqe, pool, ref), options = {'gtol': 1e-8})
+    return res
+
+
+def detailed_vqe(params, ansatz, seed, xiphos):
+    energy = t_ucc_E
+    jac = t_ucc_grad
+    hess = t_ucc_hess
+    x0 = params
+    E0 = energy(params, ansatz, xiphos.H_vqe, xiphos.pool, xiphos.ref)
+    res = scipy.optimize.minimize(energy, params, jac = jac, method = "bfgs", args = (ansatz, xiphos.H_vqe, xiphos.pool, xiphos.ref), options = {'gtol': 1e-8})
+    EF = res.fun
+    gradient = t_ucc_grad(res.x, ansatz, xiphos.H_vqe, xiphos.pool, xiphos.ref)
+    jacobian = t_ucc_jac(res.x, ansatz, xiphos.H_vqe, xiphos.pool, xiphos.ref)
+    hessian = t_ucc_hess(res.x, ansatz, xiphos.H_vqe, xiphos.pool, xiphos.ref)
+    gnorm = np.linalg.norm(gradient)
+    u, s, vh = np.linalg.svd(jacobian.todense())
+    w, v = np.linalg.eigh(hessian)
+    state = t_ucc_state(res.x, ansatz, xiphos.pool, xiphos.ref)
+    fid = ((xiphos.ed_wfns[:,0].T)@state)[0].real**2
+    string = "\nSolution Analysis:\n\n"
+    string += f"Parameters: {len(ansatz)}\n"
+    string += f"Initialization: {seed}\n"
+    string += f"Initial Energy: {E0:20.16f}\n"
+    string += f"Final Energy: {EF:20.16f}\n"
+    string += f"GNorm: {gnorm:20.16f}\n"
+    string += f"Fidelity: {fid:20.16f}\n"
+    string += f"Solution Parameters:\n"
+    for x in res.x:
+        string += f"{x},"
+    string += "\n"
+    string += f"Jacobian Singular Values:\n"
+    for sv in s:
+        string += f"{sv},"
+    string += "\n"
+    string += f"Hessian Eigenvalues:\n"
+    for sv in w:
+        string += f"{sv},"
+    string += "\n"
+    string += f"Operator/ Expectation Value/ Error:\n"
+    for key in xiphos.sym_ops.keys():
+        val = ((state.T)@(xiphos.sym_ops[key]@state))[0,0].real
+        err = val - xiphos.ed_syms[0][key]
+        string += f"{key:<6}:      {val:20.16f}      {err:20.16f}\n"
+    string += '\n\n'
+    return [res, string]
+
 def solution_analysis(L, ansatz, H_vqe, pool, ref, seeds, param_list, E0s, xiphos, guess = 'recycled'):
     Es = [L[i].fun for i in range(0, len(L))]
     xs = [L[i].x for i in range(0, len(L))]
